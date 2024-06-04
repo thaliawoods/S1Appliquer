2092e100a3eb5ece855410a841d7ad65f170ffea
thaliawoods <thaliadelano@gmail.com>
2024-06-04 17:49:41 +0200

    TroisiemeEtape

--- recette.js
+++ recette.js
@@ -17,3 +17,15 @@ console.log(ingredients.length)
 const phrase = "Pour faire des pates carbo il faut toujours avoir dans son frigo des " + ingredients 
 
 console.log(phrase)
+
+// Troisième étape : L’affichage doit se faire avec un numéro attribué à l’ordre, comme dans la liste ci-dessus. On doit revenir à la ligne pour chaque ingrédient. 
+
+let listeIngredients = "";
+for (let i = 0; i < ingredients.length; i++) {
+    listeIngredients += (i+1) + "." + ingredients[i] + "\n";
+}
+
+console.log(listeIngredients)
+
+
+
