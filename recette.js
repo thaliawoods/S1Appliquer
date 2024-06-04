3beaabb21ab91bbccfd2da4125eaeb71587fcf82
thaliawoods <thaliadelano@gmail.com>
2024-06-04 17:39:22 +0200

    PremiereEtape/DeuxiemeEtape

---
+++ recette.js
@@ -0,0 +1,19 @@
+// Première étape : La liste des ingrédients doit être stockée dans un tableau. Afficher en une phrase, le nombre d’ingrédients nécessaires pour la recette. 
+
+const ingredients = [
+    "Pâtes",
+    "Oeuf",
+    "Sel",
+    "Poivre",
+    "Lardon",
+    "Oignon",
+    "Parmesan"
+  ];
+
+console.log(ingredients.length)
+
+// Deuxième étape : Afficher la liste des ingrédients nécessaires pour la recette des pâtes carbonara. Pas de mode d’affichage imposé. 
+
+const phrase = "Pour faire des pates carbo il faut toujours avoir dans son frigo des " + ingredients 
+
+console.log(phrase)
